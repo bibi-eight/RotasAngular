e18f1c3707d7e73a600fba3a63ddfbf17b0dd08a
brenda <bbritob8@gmail.com>
2023-11-22 16:23:01 -0300

    entendendo query params

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -8,11 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CardComponent implements OnInit {
 
+  //http://localhost:4200/portfolio/{1}
   constructor(private activeRoute: ActivatedRoute)
   {
     this.activeRoute.params.subscribe(res => console.log(res))
-  }
+  
 
+  //http:localhost:4200/portfolio/1?{name=brenda&token=123}
+    this.activeRoute.queryParams.subscribe(res => console.log(res))
+
+  }
   ngOnInit(): void {
   }
 
